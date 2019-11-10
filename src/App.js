4d7b4832a2b16b8ae5de684680bdcbdc24349c87
modeverv <modeverv@gmail.com>
2019-11-10 16:13:07 +0900

    more-jsx

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import React, {Component} from 'react'
 class App extends Component {
   render(){
     //return <div>Hello world</div>;
-    return <h1>Hello world</h1>;    
+    const greeting = "aaa"
+    const dom = <h1>hi!!!</h1>
+    const dom2 = <h1>{greeting}</h1>    
+    const dom3 = <h1 className="foo">{greeting}</h1>        
+    const ipt = <input id="a" type="text" onClick={()=>{console.log("aaaa")}}/>
+    const dom4 = <div>
+      <label htmlFor="a">laaaa</label>    
+      {ipt}
+    </div>
+    const dom5 = <React.Fragment>
+      <label htmlFor="a">laaaa</label>    
+      {ipt}
+    </React.Fragment>
+
+    //return <h1>Hello world</h1>;    
+    //return dom
+    //return dom2
+    return dom5
   }
 }
 
