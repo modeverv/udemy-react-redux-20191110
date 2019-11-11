5c046a46e6802ec946e32ac272faec1b7d89ca0d
modeverv <modeverv@gmail.com>
2019-11-11 15:23:25 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,60 +1,36 @@
 import React, {Component} from 'react'
-import PropTypes from 'prop-types'
 
-/*
-class App extends Component {
-  render(){
-    //return <div>Hello world</div>;
-    const greeting = "aaa"
-    const dom = <h1>hi!!!</h1>
-    const dom2 = <h1>{greeting}</h1>    
-    const dom3 = <h1 className="foo">{greeting}</h1>        
-    const ipt = <input id="a" type="text" onClick={()=>{console.log("aaaa")}}/>
-    const dom4 = <div>
-      <label htmlFor="a">laaaa</label>    
-      {ipt}
-    </div>
-    const dom5 = <React.Fragment>
-      <label htmlFor="a">laaaa</label>    
-      {ipt}
+const App = () => {return <Counter></Counter>}
+
+
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    console.log(this.state)
+    this.state = {count : 0}
+  }
+
+  handlePlus = () => {
+    this.setState({count: this.state.count + 1})
+  }
+
+  handleMinus = () => {
+    window.console.log("handleMinus")
+    const currentCount = this.state.count
+    this.setState({count: currentCount - 1})
+  }
+
+
+  render() {
+    console.log(this.state)
+    return (
+      <React.Fragment>
+    <div>count: {this.state.count}</div>
+    <button onClick={this.handlePlus}>+1</button>
+    <button onClick={this.handleMinus}>-1</button>
     </React.Fragment>
-
-    //return <h1>Hello world</h1>;    
-    //return dom
-    //return dom2
-    return dom5
+    );
   }
 }
-*/
-const App = () => {
-  const profiles = [
-    {name: "Jiro1", age: 1},
-    {name: "Jiro2", age: 2},
-    {name: "Jiro3"},    
-  ]
-//  return <div>!!!!</div>
-  return <div>
-   <User name={"Taro1"} age={1}/>
-   <User name={"Taro2"} age={2}/>
-   <User name={"Taro3"} age={3}/>
-   {
-     profiles.map((p,index) => {
-       return <User name={p.name} age={p.age} key={index}/>
-     })
-   }
-  </div>
-}
-// 子孫へ向かってデータを送り込んでいく
-const User = (props) => {
-  return <p>I'm {props.name}, {props.age} yeas old.</p>
-}
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number/*.isRequired*/
-}
- 
-//User.defaultProps = {
-//  age: 100
-//}
 export default App;
