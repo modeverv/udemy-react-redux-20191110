41a0b56d9246bcb1b2c4a9a3a52d1e0e1cb9d425
modeverv <modeverv@gmail.com>
2019-11-13 16:20:26 +0900

    rename

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducers'
 import './index.css';
-import App from './components/App';
+import EventsIndex from './components/events_index';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducer)
@@ -12,7 +12,7 @@ const store = createStore(reducer)
 
 ReactDOM.render(
 <Provider store={store}>
-<App />    
+<EventsIndex />    
 </Provider>
 , document.getElementById('root'));
 
