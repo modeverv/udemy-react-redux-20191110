f95ed03b13bf6493c41116c90ce0fc98f036d9e1
modeverv <modeverv@gmail.com>
2019-11-13 18:38:18 +0900

    form

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,12 @@ import axios from 'axios'
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERY_STRING = '?token=token123'
 export const READ_EVENTS = "READ_EVENTS"
+export const CREATE_EVENT = "CREATE_EVENT"
 export const readEvents = () => async dispatch => {
     const response = await axios.get(`${ROOT_URL}/events${QUERY_STRING}`)
-    //console.log(response)
     dispatch({type: READ_EVENTS, response})
+}
+export const postEvent = values => async dispatch => {
+    const response = await axios.post(`${ROOT_URL}/events${QUERY_STRING}`, values)
+    dispatch({type: CREATE_EVENT, response})
 }
